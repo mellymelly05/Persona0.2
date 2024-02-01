4abb92c7d91ad89e5971521829f81ceea41f7ae4
melis <melis@LAPTOP-N1S6S5M8>
2024-02-01 16:43:47 +0100

    Add project files.

---
+++ Persona0.2/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona0._2
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public int Eta { get; set; }
+
+        public Persona(string Nome,int Eta ) 
+        {
+            this.Nome = Nome;
+            this.Eta = Eta;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome},{Eta}";
+        }
+    }
+}
---
+++ Persona0.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona0._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Persona> Persone = new List<Persona>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Persone.Add(new Persona(Console.ReadLine(),int.Parse(Console.ReadLine())));   
+            }
+
+            for(int i = 0;i <Persone.Count;i++)
+            {
+                if (Persone[i].Eta>=18)
+                {
+                    Console.WriteLine(Persone[i].ToString());
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
